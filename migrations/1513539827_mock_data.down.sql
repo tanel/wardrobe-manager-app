ecb872d2d5214ee364907fa1c62559b152d954a3
Tanel Lebedev <tanel.lebedev@gmail.com>
2017-12-17 21:46:21 +0200

    Add mock user

---
+++ migrations/1513539827_mock_data.down.sql
@@ -0,0 +1,2 @@
+delete from users
+	where id = '18f25d1b-dd0a-4889-9610-d103164c2f2e';
---
+++ migrations/1513539827_mock_data.up.sql
@@ -0,0 +1,2 @@
+insert into users(id, email, password_hash, created_at)
+values('18f25d1b-dd0a-4889-9610-d103164c2f2e', 'test@test.com', '$2a$10$ZX3SHCjOdY2iU827kPViw.Qkgts1OhrYob3EQD6dvXgHraTMe/NC2', current_timestamp);
