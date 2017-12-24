b7e38c60dea71698fdee7985eaf6141e945b421b
Tanel Lebedev <tanel.lebedev@gmail.com>
2017-12-24 23:23:45 +0200

    Item confirmation page test

---
+++ controller/get_confirm_delete_item_test.go
@@ -0,0 +1,36 @@
+package controller
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/assert"
+	"github.com/tanel/wardrobe-manager-app/db"
+	"github.com/tanel/wardrobe-manager-app/session"
+)
+
+func Test_GetConfirmDeleteItem_ReturnsResponse_InCaseOfSuccess(t *testing.T) {
+	// Arrange
+	assert.NoError(t, db.Connect())
+	const itemID = "9924B052-656C-4A99-BE22-5CF0CC2FD287"
+	ps := httprouter.Params{
+		httprouter.Param{
+			Key:   "id",
+			Value: itemID,
+		},
+	}
+
+	req := httptest.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	const userID = "3E874918-F54E-49A3-A635-321437A85F00"
+	assert.NoError(t, session.SetUserID(w, req, userID))
+
+	// Act
+	GetConfirmDeleteItem(w, req, ps)
+
+	// Assert
+	resp := w.Result()
+	assert.Equal(t, 200, resp.StatusCode)
+}
