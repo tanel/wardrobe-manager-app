702c353298a173bfa18c5700fd14c826c9af7eee
Tanel Lebedev <tanel.lebedev@gmail.com>
2017-12-27 06:31:37 +0200

    Refactor

--- controller/get_items.go
+++ controller/get_items.go
@@ -13,40 +13,6 @@ import (
 	"github.com/tanel/wardrobe-manager-app/ui"
 )
 
-type filters struct {
-	category string
-	brand    string
-	color    string
-	outfitID string
-}
-
-func newFilters(w http.ResponseWriter, r *http.Request) (*filters, error) {
-	var result filters
-	var err error
-
-	result.category, err = handleParam(w, r, "category")
-	if err != nil {
-		return nil, errors.Annotate(err, "handling category param failed")
-	}
-
-	result.brand, err = handleParam(w, r, "brand")
-	if err != nil {
-		return nil, errors.Annotate(err, "handling brand param failed")
-	}
-
-	result.color, err = handleParam(w, r, "color")
-	if err != nil {
-		return nil, errors.Annotate(err, "handling color param failed")
-	}
-
-	result.outfitID, err = handleParam(w, r, session.AddToOutfitID)
-	if err != nil {
-		return nil, errors.Annotate(err, "handling outfit ID param failed")
-	}
-
-	return &result, nil
-}
-
 // GetItems renders items page
 func GetItems(w http.ResponseWriter, r *http.Request, ps httprouter.Params, userID string) {
 	f, err := newFilters(w, r)
@@ -153,4 +119,38 @@ func handleParam(w http.ResponseWriter, r *http.Request, name string) (string, e
 	}
 
 	return value, nil
+}
+
+type filters struct {
+	category string
+	brand    string
+	color    string
+	outfitID string
+}
+
+func newFilters(w http.ResponseWriter, r *http.Request) (*filters, error) {
+	var result filters
+	var err error
+
+	result.category, err = handleParam(w, r, "category")
+	if err != nil {
+		return nil, errors.Annotate(err, "handling category param failed")
+	}
+
+	result.brand, err = handleParam(w, r, "brand")
+	if err != nil {
+		return nil, errors.Annotate(err, "handling brand param failed")
+	}
+
+	result.color, err = handleParam(w, r, "color")
+	if err != nil {
+		return nil, errors.Annotate(err, "handling color param failed")
+	}
+
+	result.outfitID, err = handleParam(w, r, session.AddToOutfitID)
+	if err != nil {
+		return nil, errors.Annotate(err, "handling outfit ID param failed")
+	}
+
+	return &result, nil
 }
