fb95bb83d26631b85d63aa7582fc3c8acb675d3e
Tanel Lebedev <tanel.lebedev@gmail.com>
2018-03-19 00:12:45 +0200

    Remove default env. values

--- main.go
+++ main.go
@@ -10,24 +10,15 @@ import (
 	"github.com/tanel/webapp/session"
 )
 
-const port = ":8080"
-
-// nolint: gas
-const unsecureSessionSecret = "7215B41B-812A-4FBF-9A5D-D3ACDE950C12"
-
 func main() {
 	databaseConnection := db.Connect("wardrobe", "wardrobe")
 
-	sessionSecret := unsecureSessionSecret
-	if secret := os.Getenv("WARDROBE_SESSIONSECRET"); secret != "" {
-		sessionSecret = secret
-	} else {
-		log.Println("Warning: set WARDROBE_SESSIONSECRET")
-	}
-
+	sessionSecret := os.Getenv("WARDROBE_SESSIONSECRET")
 	sessionStore := session.New(sessionSecret, "wardrobe-session")
 
 	r := router.New(databaseConnection, sessionStore)
+
+	port := ":" + os.Getenv("WARDROBE_PORT")
 
 	log.Println("Server starting at http://localhost" + port)
 
